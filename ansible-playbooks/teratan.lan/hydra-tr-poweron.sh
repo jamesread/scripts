9b80437f02f00296407294605742a58352b6cbac
jamesread <contact@jwread.com>
2020-01-27 12:33:08 +0000

    .

--- ansible-playbooks/teratan.lan/hydra-tr-poweron.sh
+++ ansible-playbooks/teratan.lan/hydra-tr-poweron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./teratan-supermicro.yml -e SERVER_BMC=192.168.66.25 -e SERVER_IP=192.168.66.1
+./teratan-supermicro.yml -e SERVER_BMC=hydra-bl-bmc -e SERVER_IP=192.168.66.1
---
+++ ansible-playbooks/vmcreator-libvirt/createCentosUpsilon.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./create.yml -ecount=3 -eramMb=8192 -evmTitle=drone
+
