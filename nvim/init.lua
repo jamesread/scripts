c0938e89e6f8580819f5f51536fafdd5d2489629
jamesread <contact@jread.com>
2026-06-06 12:27:26 +0100

    kitty config, nvim, etc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,14 @@
 vim.o.number = true
-vim.o.conceallevel = 2
+--vim.o.conceallevel = 2
 vim.o.relativenumber = true
-vim.o.termguicolors = false
+vim.o.termguicolors = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.wrap = false
+
+vim.opt.diffopt:append("algorithm:histogram")
+vim.opt.diffopt:append("linematch:60")
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "javascript",
@@ -40,9 +43,20 @@ vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = true, sil
 vim.api.nvim_set_keymap("n", "<S-K>", "i[text](url)<Esc>2hi", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<leader>k", '"sy<ESC>`<v`>s[<C-r>s](url)<Left>', { noremap = true, silent = true })
 
-vim.cmd.set("background=light")
 vim.cmd.colorscheme("vim")
+vim.cmd.set("background=light")
+vim.api.nvim_set_hl(0, "Normal",		{ bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC",		{ bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn",	{ bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer",	{ bg = "none" })
+vim.api.nvim_set_hl(0, "Statement",		{ bg = "none",		fg = "#BB7721" })
+vim.api.nvim_set_hl(0, "Constant",		{ bg = "none",		fg = "#D60429" })
+vim.api.nvim_set_hl(0, "Identifier",	{ bg = "none",		fg = "#000000" })
+vim.api.nvim_set_hl(0, "Type",			{ bg = "none",		fg = "#2E8B57" })
+vim.api.nvim_set_hl(0, "Special",		{ bg = "none",		fg = "#bb0000", bold = false })
+vim.api.nvim_set_hl(0, "PreProc",		{ bg = "none",		fg = "#6A0DAD", bold = true })
 
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#888888" })
 
 local cmp = require'cmp'
 
@@ -59,6 +73,10 @@ cmp.setup({
 
 require("refactoring").setup()
 require("nvim-treesitter").setup()
+require("nvim-treesitter.configs").setup({ highlight = { enable = true } })
+
+
+require("diffview").setup()
 
 require("lualine").setup{
 	options = {
--- nvim/lua/diffcolors.lua
+++ nvim/lua/diffcolors.lua
@@ -1,14 +1,11 @@
 local M = {}
 
 function M.apply()
---	vim.api.nvim_set_hl(0, "DiffAdd",    { bg = "#203b2a", fg = "#990000" })
---	vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#3f1d1d", fg = "#00ff00" })
-	vim.api.nvim_set_hl(0, "DiffChange", { bg = "#990000", fg = "#ffff00" })
-	vim.api.nvim_set_hl(0, "DiffChanged", { bg = "#990000", fg = "#ffff00" })
---	vim.api.nvim_set_hl(0, "DiffText",   { bg = "#3a4a7a", fg = "#099000", bold = true })
-
+	vim.api.nvim_set_hl(0, "DiffAdd",    { bg = "#CDEB8B" })
+	vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#EB8B9D" })
+	vim.api.nvim_set_hl(0, "DiffChange", { bg = "#EBC78B" })
+	vim.api.nvim_set_hl(0, "DiffText",   { bg = "#FFB977" })
+	vim.api.nvim_set_hl(0, "DiffviewFilePanelFileName",  { bg = "#CDEB8B" })
 end
-
--- #cake
 
 return M
